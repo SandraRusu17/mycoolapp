a536d381afccbc0500ffdbc0698c49fe02b04b80
SandraRusu <sandra.rusu.ext@extendaretail.com>
2022-07-25 18:12:44 +0300

    Spring Boot - Injecting Custom Application Properties

--- mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,5 +1,6 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,18 @@ import java.time.LocalDateTime;
 
 @RestController
 public class FunRestController {
+    // inject properties for: coach.name and team.name
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach " + coachName + ", Team name:" + teamName;
+    }
 
     @GetMapping("/")
     public String sayHello(){
