46d2e2409cfbe32ff61707dd3e9f2129f680db1c
SandraRusu <sandra.rusu.ext@extendaretail.com>
2022-07-27 19:04:37 +0300

    Developed the DAO Interface and Implementation

---
+++ mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/dao/EmployeeDAO.java
@@ -0,0 +1,10 @@
+package com.luv2code.springboot.demo.mycoolapp.dao;
+
+import com.luv2code.springboot.demo.mycoolapp.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    public List<Employee> findAll();
+}
