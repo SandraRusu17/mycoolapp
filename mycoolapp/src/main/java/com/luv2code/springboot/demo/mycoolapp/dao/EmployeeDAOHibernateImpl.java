1fd239b4b6a6f19713b4958f2e79f80568614571
SandraRusu <sandra.rusu.ext@extendaretail.com>
2022-08-09 15:44:37 +0300

    Create additional CRUD Methods for DAO

--- mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/dao/EmployeeDAOHibernateImpl.java
+++ mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/dao/EmployeeDAOHibernateImpl.java
@@ -26,7 +26,6 @@ public class EmployeeDAOHibernateImpl implements EmployeeDAO{
     }
 
     @Override
-    @Transactional
     public List<Employee> findAll() {
 
         // get the current hibernate session
@@ -41,5 +40,43 @@ public class EmployeeDAOHibernateImpl implements EmployeeDAO{
         // return the results
 
         return employees;
+    }
+
+    @Override
+    public Employee findById(int theId) {
+
+        // get the current hibernate session
+        Session currentSession = entityManager.unwrap(Session.class);
+
+        // get the employee
+        Employee theEmployee = currentSession.get(Employee.class, theId);
+
+        // return the employee
+        return theEmployee;
+    }
+
+    @Override
+    public void save(Employee theEmployee) {
+
+        // get the current hibernate session
+        Session currentSession = entityManager.unwrap(Session.class);
+
+        // save employee
+        // remember if id=0 then save/insert else update
+        currentSession.saveOrUpdate(theEmployee);
+
+    }
+
+    @Override
+    public void deleteById(int theId) {
+
+        // get the current hibernate session
+        Session currentSession = entityManager.unwrap(Session.class);
+
+        // delete object with primary key
+        Query theQuery = currentSession.createQuery("delete from Employee where id=:employeeId");
+        theQuery.setParameter("employeeId", theId);
+        theQuery.executeUpdate();
+
     }
 }
